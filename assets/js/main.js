8227423ac4a9a912e1ad1e42a5f608eb8ffd088a
unknown <kbpraveen1999@gmail.com>
2023-01-31 16:07:46 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,226 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction));
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header');
+
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+
+skillsHeader.forEach((each) => {
+    each.addEventListener('click', toggleSkills);
+});
+
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]');
+
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active');
+        });
+        target.classList.add('qualification__active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__acctive');
+        });
+        tab.classList.add('qualification__active');
+    });
+});
+
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close');
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+};
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    });
+});
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        });
+    });
+});
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let slides = document.querySelectorAll(".slide");
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+
+let index = 0;
+
+for (let i = 0; i < slides.length; i++) {
+    if (slides[i].classList.contains("active")) {
+        index = i;
+    }
+}
+
+prev.onclick = prevSlide;
+next.onclick = nextSlide;
+
+
+function prevSlide() {
+    slides[index].classList.remove("active");
+    index--;
+    console.log(index);
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+    slides[index].classList.add("active");
+}
+
+function nextSlide() {
+    slides[index].classList.remove("active");
+    index++;
+    console.log(index);
+    if (index === slides.length) {
+        index = 0;
+    }
+    console.log(index);
+    slides[index].classList.add("active");
+}
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const nav = document.getElementById('header');
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+const mainSectionImg = document.getElementById('mainSectionImg');
+const aboutSectionImg = document.getElementById('aboutSectionImg');
+
+const mainSecDarkImg = `assets/img/main_dark.png`;
+const mainSecLightImg = "assets/img/Pkb777_pixarcool_boysmilenature_background_4c55880e-db90-48b0-8a5c-ea3b5d44dd88.png";
+
+const aboutSectionDarkImg = "assets/img/Pkb777_dark_theme_0d06337c-0d4a-4bb7-b5df-31763a969d8d.png";
+const aboutSectionlightImg = "assets/img/Pkb777_pixarcool_boyprofessionalnature_background_96471a85-4a85-4be7-9eb6-0118bdad3c1f.png";
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+
+    console.log(mainSectionImg.href.baseVal);
+    console.log(mainSectionImg.href.animVal);
+    console.log(aboutSectionImg);
+}
+
+if (selectedTheme === "dark") {
+    mainSectionImg.href.baseVal = mainSecDarkImg;
+    aboutSectionImg.src = aboutSectionDarkImg;
+} else {
+    mainSectionImg.href.baseVal = mainSecLightImg;
+    aboutSectionImg.src = aboutSectionlightImg;
+}
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+    if (document.body.classList.value === "dark-theme") {
+        mainSectionImg.href.baseVal = mainSecDarkImg;
+        aboutSectionImg.src = aboutSectionDarkImg;
+    } else {
+        mainSectionImg.href.baseVal = mainSecLightImg;
+        aboutSectionImg.src = aboutSectionlightImg;
+    }
+});
